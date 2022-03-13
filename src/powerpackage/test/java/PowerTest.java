d11a8d1c4f412687bd57714053fc31dfab16d337
Varshni11 <varshnin3@gmail.com>
2022-03-13 15:43:32 +0530

    Improved naming conventions

---
+++ src/powerpackage/test/java/PowerTest.java
@@ -0,0 +1,31 @@
+package powerpackage;
+
+
+
+import org.junit.jupiter.api.Test;
+public class PowerTest {
+
+    @Test
+    public void oneRaisedToOneIsOne() {
+
+        assert PowerFinder.of(1,1) == 1;
+    }
+
+    @Test
+    public void twoRaisedToOneIsTwo() {
+
+        assert PowerFinder.of(2,1) == 2;
+    }
+
+    @Test
+    public void twoRaisedToTwoIsFour() {
+
+        assert PowerFinder.of(2, 2) == 4;
+    }
+
+    @Test
+    public void threeRaisedToTwoIsNine() {
+
+        assert PowerFinder.of(3, 2) == 3*3;
+    }
+}
